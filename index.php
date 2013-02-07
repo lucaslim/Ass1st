d811f165b813cb35f7987ecebe69beecfec32299
Michael <mandarino013@hotmail.com>
2013-02-07 09:43:22 -0500

    test

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 /*
  *---------------------------------------------------------------
- * APPLICATION ENVIRONMENT
+ * APPLICATION ENVIRONMENTS
  *---------------------------------------------------------------
  *
  * You can load different configurations depending on your
