9643a011a082e6c138dc69c5f9f6937d0f5cdc32
Lucas Lim <lucas@nuxbox.me>
2013-03-11 15:17:20 -0400

    Fix login_header_view

--- application/views/login_header_view.php
+++ application/views/login_header_view.php
@@ -38,18 +38,6 @@ $(document).ready(function() {
 	});
 </script>
 <?php echo form_open('login/login_verify', array('id' => 'login_form')); ?>
-<<<<<<< HEAD:application/views/login_view.php
-	<fieldset>
-		<input type="text" placeholder="Email" id="email" name="email"/>
-		<input type="password" placeholder="Password" id="password" name="password"/>
-		<input type="submit" class="btn" value="Login"/>
-		<label class="checkbox">
-			<input type="checkbox">	Remember Me 
-		</label>
-		<div id="error_box" title="Error">
-			<div id="error_message"></div>
-		</div>
-=======
 <fieldset>
 	<input type="text" placeholder="Email" id="email" name="email"/>
 	<input type="password" placeholder="Password" id="password" name="password"/>
@@ -60,6 +48,5 @@ $(document).ready(function() {
 	<div id="error_box" title="Incorrect Login">
 		<div id="error_message"></div>
 	</div>
->>>>>>> Added Header Login:application/views/login_header_view.php
 </fieldset>
 </form>
