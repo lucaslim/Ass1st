1947df6729595492cd983f8f8c05de399606cf06
Michael <mandarino013@hotmail.com>
2013-03-12 15:08:21 -0400

    css login
    standarized fonts and .em

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,6 +3,13 @@
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!-->
+<!--[if gte IE 9]>
+  <style type="text/css">
+    .gradient {
+       filter: none;
+    }
+  </style>
+<![endif]-->
 <html class="no-js">
 	<!--<![endif]-->
 	<head>
