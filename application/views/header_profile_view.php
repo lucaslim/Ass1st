b362d31c4dfdf9dc01c807cfc96867d02efbd95f
Michael <mandarino013@hotmail.com>
2013-03-17 16:34:27 -0400

    header updated

--- application/views/header_profile_view.php
+++ application/views/header_profile_view.php
@@ -1,18 +1,22 @@
-<table style="float:right;">
-	<tr>
-		<td>
-			<!-- team logo -->
-			<img style="height:24px; width:24px; border: 2px solid black;" src="<?php echo $picture; ?>">
-		</td>
-		<td>
-			<!-- player logo -->
-			<img style="height:24px; width:24px; border: 2px solid black;" src="<?php echo $picture; ?>">
-		</td>
-		<td>
-			<?php echo $full_name; ?>
-		</td>
-		<td>
-			<a href="#<?php echo $id; ?>">Profile</a> | <a href="<?php echo base_url(); ?>logout">Log Out</a>
-		</td>
-	</tr>
-</table>
+<div id="header_profile_view">
+	<table>
+		<tr>
+			<td>
+				<!-- team logo -->
+				<img id="img_team" src="<?php echo base_url(); ?>assets/images/temp/wolverine_login.jpg"/>
+			</td>
+			<td>
+				<!-- player logo -->
+				<img id="img_player" src="<?php echo $picture; ?>">
+			</td>
+			<td>
+				<div id="usr_name">
+					<a href src="#"><?php echo $full_name; ?>&nbsp;<i class="icon-caret-down icon-large"></i></a>
+				</div>
+				<div id="usr_options">
+					<a href="#<?php echo $id; ?>">Profile</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="<?php echo base_url(); ?>logout">Log Out</a>
+				</div>
+			</td>
+		</tr>
+	</table>
+</div>
