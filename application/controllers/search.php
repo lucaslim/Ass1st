673b90ca1dfc5f31a82675102de179734af39d86
Jymele <jymele@yahoo.fr>
2013-04-22 14:56:31 -0400

    made search textbox functional

---
+++ application/controllers/search.php
@@ -0,0 +1,45 @@
+<?php
+if (!defined('BASEPATH')) exit('no direct script access allowed');
+
+/**
+ * Assist
+ *
+ * This is the controller for the index page
+ *
+ * @package		Assist
+ * @author		Team Assist
+ */
+
+// --------------------------------------------------------------------
+
+class Search extends CI_Controller {
+
+	/**
+	 * Constructor for the Pages Class
+	 *
+	 * Load required model, helper, library class file.
+	 *
+	 */
+	function __construct() {
+		parent::__construct();
+
+		$this -> load -> helper('template');
+		$this -> load -> helper(array('form', 'url'));
+		$this -> load -> helper('login_helper');
+	}	
+
+	public function index()
+	{
+		$this-> load -> model('search_model');
+		$data['search'] = $this->input->post('q');
+		$data['login_header'] = set_login_header(); //get from template_helper.php
+
+		$data['query'] = $this->search_model->get_search($this->input->post('q'));
+
+		$this -> load -> view('templates/header', $data);
+		$this-> load -> view('search_view',$data);
+		$this -> load -> view('templates/footer', $data);
+
+	}
+}
+?>
