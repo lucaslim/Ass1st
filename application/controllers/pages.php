c85a05b5e946fdd61242d5e894e05acf8edf989e
Michael <mandarino013@hotmail.com>
2013-04-22 16:50:30 -0400

    2

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -240,7 +240,7 @@ class Pages extends CI_Controller {
 
 		$this -> load -> view( 'templates/header', $data );
 		$this -> load -> view( 'pages/user_profile.php', $data );
-		$this -> load -> view( 'templates/footer' );
+		$this -> load -> view( 'templates/footer' ); 
 	}
 
 	// --------------------------------------------------------------------
