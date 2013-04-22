42f42830dee273f5be43b9800884bf881c976cee
Jymele <jymele@yahoo.fr>
2013-04-22 18:10:30 -0400

    images

--- application/controllers/admin/media.php
+++ application/controllers/admin/media.php
@@ -40,7 +40,7 @@ class Media extends Admin_Controller {
 	{
 		$config['upload_path'] = './uploads/';
 		$config['allowed_types'] = 'gif|jpg|png';
-		$config['max_size']	= '100';
+		$config['max_size']	= '250';
 		$config['max_width']  = '1024';
 		$config['max_height']  = '768';
 
