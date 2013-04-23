6a517097443ba6f09a84897c3afa2519af324db1
Neil Fenton <neilff@neil-linux-laptop.(none)>
2013-04-23 06:19:04 -0400

    ffsdfsfsd

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -43,7 +43,6 @@
         <script src="<?php echo base_url(); ?>/style/bootstrap/js/vendor/jquery-1.9.1.min.js"></script>        
 
         <script src="<?php echo base_url(); ?>/style/bootstrap/js/vendor/bootstrap.min.js"></script>
-        <script src="<?php echo base_url(); ?>/style/bootstrap/js/vendor/bootstrap.min.js"></script>
 
 		<script type="text/javascript" src="<?php echo base_url(); ?>script/chat.js"></script>
 		<script src="http://code.jquery.com/ui/1.10.1/jquery-ui.js"></script>
