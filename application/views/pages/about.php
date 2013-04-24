dca172362659f7f625d0c8b199a343db9451c99d
Jymele <jymele@yahoo.fr>
2013-04-24 19:57:25 -0400

    about us page

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -5,17 +5,73 @@
 	<!-- New Row
 	====================================================================== -->
 	<div class="row-fluid">
-
+		<div class="span12">
+			<img src="<?= base_url()?>/uploads/teamassist.jpg" alt="Team Assist" title="Team Assist" class="span12" />
+		</div>
+	</div>
+	<div class="row-fluid">
 		<!-- Place Main Content Here -->
-		<div id="leftContent" class="span8">
+		<div id="leftContent" class="span4">
+			<img src="<?= base_url()?>/uploads/identity.png" alt="Team Assist" title="Team Assist" /><br /><!--team logo-->
+			image logo
+		</div>
+		<div id="rightContent" class="span8">
 			<legend>About</legend>
-			<p>Currently under development</p>
-		</div>
+			<!-- <p>Currently under development</p> -->
+			<p style="text-indent: 45px">Project Assist is a web application that is an application website that make it easier for sports leagues of all skills level and ages to manage their website. Assist is designed for any sports leagues to implement. It takes your competitive athletes and gives them a whole new outlet to raise the bar and continue to challenge themselves—and other competitors around the world. Currently most leagues are updating stats and information manually, or, unfortunately, they are not using dynamic data to their advantage. We want to make an application that simplifies data input and makes it easier and more engaging for our clients to access. </p>
 
-		<!-- Place Sidebar Content Here -->
-		<div id="rightContent" class="span4">
-			<p>Empty</p>
+			<p style="text-indent: 45px">Developed by Team Assist, this essential website is asset to any well-established sports leagues and all up and coming ones. Designed with the intentions of reusability, strengthened by unique and dynamic features, allowing for daily interactions; the Assist website is expected to an instant hit. Assist is constructed by athletes of athletes of any skill level and is expected launched date of early May 2013.</p>
 		</div>
+	</div>
+	<hr>
+	<div class="row-fluid">
+		<div class="span2">
+			<img src="<?= base_url()?>/uploads/identity.png" alt="Team Assist" title="Team Assist" />
+		</div>
+		<div class="span10">
+			<strong>Michael Mandarino – Developer &amp; Project Leader</strong>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus elit tortor, ultrices vel consectetur quis, facilisis ut mauris. Sed pulvinar diam rhoncus augue vestibulum varius. Morbi libero metus, adipiscing a tristique at, vulputate ac felis. Sed faucibus nisi id eros fermentum luctus. Nullam sit amet neque augue. Pellentesque ornare arcu non libero tincidunt tincidunt. Nam faucibus interdum erat, a ornare diam cursus id.</p>
 
+			<p>Proin sit amet mauris nisl. Nulla facilisi. Morbi id mi sapien, vitae fermentum mauris. Sed metus nunc, convallis a egestas vitae, tristique ut nisl. Suspendisse convallis vestibulum hendrerit. Duis quis est nulla. Integer rhoncus mauris sit amet diam varius cursus. Integer blandit vehicula quam, non consectetur dui ultrices eget. Mauris dictum tempor diam, eget tempus purus condimentum ac. Nullam eleifend congue lectus, eget ornare enim tincidunt eu. Suspendisse potenti. Cras condimentum bibendum sapien, laoreet porta metus dignissim id. Donec auctor sapien quis sapien porta ut pretium ipsum tempor. Cras consectetur odio vel ligula sagittis pellentesque. Nullam malesuada urna vel felis ultrices sed interdum magna ornare. Pellentesque pulvinar, justo ut luctus congue, lectus sem egestas lorem, at fringilla libero diam sit amet velit.</p>
+		</div>
+	</div>
+	<hr>
+	<div class="row-fluid">
+		<div class="span2">
+			<img src="<?= base_url()?>/uploads/identity.png" alt="Team Assist" title="Team Assist" />
+		</div>
+		<div class="span10">
+			<strong>Neil Fenton – Developer </strong>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus elit tortor, ultrices vel consectetur quis, facilisis ut mauris. Sed pulvinar diam rhoncus augue vestibulum varius. Morbi libero metus, adipiscing a tristique at, vulputate ac felis. Sed faucibus nisi id eros fermentum luctus. Nullam sit amet neque augue. Pellentesque ornare arcu non libero tincidunt tincidunt. Nam faucibus interdum erat, a ornare diam cursus id.</p>
+
+			<p>Proin sit amet mauris nisl. Nulla facilisi. Morbi id mi sapien, vitae fermentum mauris. Sed metus nunc, convallis a egestas vitae, tristique ut nisl. Suspendisse convallis vestibulum hendrerit. Duis quis est nulla. Integer rhoncus mauris sit amet diam varius cursus. Integer blandit vehicula quam, non consectetur dui ultrices eget. Mauris dictum tempor diam, eget tempus purus condimentum ac. Nullam eleifend congue lectus, eget ornare enim tincidunt eu. Suspendisse potenti. Cras condimentum bibendum sapien, laoreet porta metus dignissim id. Donec auctor sapien quis sapien porta ut pretium ipsum tempor. Cras consectetur odio vel ligula sagittis pellentesque. Nullam malesuada urna vel felis ultrices sed interdum magna ornare. Pellentesque pulvinar, justo ut luctus congue, lectus sem egestas lorem, at fringilla libero diam sit amet velit.</p>
+		</div>
+	</div>
+	<hr>
+	<div class="row-fluid">
+		<div class="span2">
+			<img src="<?= base_url()?>/uploads/identity.png" alt="Team Assist" title="Team Assist" />
+		</div>
+		<div class="span10">
+			<strong>Neil Fenton – Developer</strong>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus elit tortor, ultrices vel consectetur quis, facilisis ut mauris. Sed pulvinar diam rhoncus augue vestibulum varius. Morbi libero metus, adipiscing a tristique at, vulputate ac felis. Sed faucibus nisi id eros fermentum luctus. Nullam sit amet neque augue. Pellentesque ornare arcu non libero tincidunt tincidunt. Nam faucibus interdum erat, a ornare diam cursus id.</p>
+
+			<p>Proin sit amet mauris nisl. Nulla facilisi. Morbi id mi sapien, vitae fermentum mauris. Sed metus nunc, convallis a egestas vitae, tristique ut nisl. Suspendisse convallis vestibulum hendrerit. Duis quis est nulla. Integer rhoncus mauris sit amet diam varius cursus. Integer blandit vehicula quam, non consectetur dui ultrices eget. Mauris dictum tempor diam, eget tempus purus condimentum ac. Nullam eleifend congue lectus, eget ornare enim tincidunt eu. Suspendisse potenti. Cras condimentum bibendum sapien, laoreet porta metus dignissim id. Donec auctor sapien quis sapien porta ut pretium ipsum tempor. Cras consectetur odio vel ligula sagittis pellentesque. Nullam malesuada urna vel felis ultrices sed interdum magna ornare. Pellentesque pulvinar, justo ut luctus congue, lectus sem egestas lorem, at fringilla libero diam sit amet velit.</p>
+		</div>
+	</div>
+	<hr>
+	<div class="row-fluid">
+		<div class="span2">
+			<img src="<?= base_url()?>/uploads/identity.png" alt="Team Assist" title="Team Assist" />
+		</div>
+		<div class="span10">
+			<strong>Joel Ymele – Developer</strong>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus elit tortor, ultrices vel consectetur quis, facilisis ut mauris. Sed pulvinar diam rhoncus augue vestibulum varius. Morbi libero metus, adipiscing a tristique at, vulputate ac felis. Sed faucibus nisi id eros fermentum luctus. Nullam sit amet neque augue. Pellentesque ornare arcu non libero tincidunt tincidunt. Nam faucibus interdum erat, a ornare diam cursus id.</p>
+
+			<p>Proin sit amet mauris nisl. Nulla facilisi. Morbi id mi sapien, vitae fermentum mauris. Sed metus nunc, convallis a egestas vitae, tristique ut nisl. Suspendisse convallis vestibulum hendrerit. Duis quis est nulla. Integer rhoncus mauris sit amet diam varius cursus. Integer blandit vehicula quam, non consectetur dui ultrices eget. Mauris dictum tempor diam, eget tempus purus condimentum ac. Nullam eleifend congue lectus, eget ornare enim tincidunt eu. Suspendisse potenti. Cras condimentum bibendum sapien, laoreet porta metus dignissim id. Donec auctor sapien quis sapien porta ut pretium ipsum tempor. Cras consectetur odio vel ligula sagittis pellentesque. Nullam malesuada urna vel felis ultrices sed interdum magna ornare. Pellentesque pulvinar, justo ut luctus congue, lectus sem egestas lorem, at fringilla libero diam sit amet velit.</p>
+		</div>
+	</div>
+				
+		</div>
 	</div>
 </div>
