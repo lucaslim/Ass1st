44b86a348908251afc86ecd2fec3655a113ed669
Jymele <jymele@yahoo.fr>
2013-05-09 22:25:10 -0400

    chat should be fixed
    Might need to try it with one of you guys tomorrow

--- application/controllers/chat.php
+++ application/controllers/chat.php
@@ -6,31 +6,32 @@ class Chat extends CI_Controller {
 	{
 		parent::__construct();	
 		
-		$this->load->model('chat_model');	
+		$this->load->model('chat_model');
 	}
 	
 	function index()
 	{
 		/* send in chat id and user id */
 		
-
+		//$data['chat_id'] = 1;
+		
 		// check they are logged in
-		if (! $this->session->userdata('authorized')) {
-			redirect('index.php');
-		}
-		
-		
-		$user_data = $this -> session -> userdata('authorized');
-		$data['user_id'] = $user_data['id'];
-		$data['chat_id'] = $user_data['team'][0];
+		// if (! $this->session->userdata('logged_in')) {
+		// 	redirect('user/login');
+		// }
+		$this->view_data['chat_id'] = 1;
 
+
+		 $user_data = $this->session->userdata('authorized');
+		// $data['user_id'] = $user_data['id'];
+
+		 $this->view_data['user_id'] = $user_data['id'];
 		
-		$this -> session -> set_userdata('last_chat_message_id_' . $data['chat_id'], 0);
+		$this->session->set_userdata('last_chat_message_id_' . $this->view_data['chat_id'], 0);
 		
-		$data['page_title'] = '';
-		$data['page_content'] = 'view_chat';
-		$this->load->view('view_main', $data);	
 		
+		// $this->load->view('view_chat', $data);
+		$this->load->view('view_chat', $this->view_data);
 	}
 	
 	
@@ -44,9 +45,9 @@ class Chat extends CI_Controller {
 		 * chat_message_content
 		 * 		 * 
 		 */
+		
 		$user_data = $this->session->userdata('authorized');
-
-		$chat_id = $user_data['team'][0];
+		$chat_id = $this->input->post('chat_id');
 		$user_id = $user_data['id'];
 
 		$chat_message_content = $this->input->post('chat_message_content', TRUE);
@@ -59,9 +60,8 @@ class Chat extends CI_Controller {
 	
 	function ajax_get_chat_messages()
 	{
-
-		$chat_id = $user_data['team'][0];
-
+		$chat_id = $this->input->post('chat_id');
+		
 		echo $this->_get_chat_messages($chat_id);
 	}
 	
@@ -86,7 +86,7 @@ class Chat extends CI_Controller {
 			{
 				$li_class = ($this->session->userdata('user_id') == $chat_message->user_id) ? 'class="by_current_user"' : '';
 				
-				$chat_messages_html .= '<li ' . $li_class . '>' . '<span class="chat_message_header">' . $chat_message->chat_message_timestamp . ' by ' . $chat_message->FirstName . '</span><p class="message_content">' .  $chat_message->chat_message_content . '</p></li>';
+				$chat_messages_html .= '<li>' . '<span class="chat_message_header">' . $chat_message->chat_message_timestamp . ' by ' . $chat_message->FirstName . ' ' . $chat_message->LastName . '</span><p class="message_content">' .  $chat_message->chat_message_content . '</p></li>';
 			}
 			
 			$chat_messages_html .= '</ul>';
@@ -99,7 +99,7 @@ class Chat extends CI_Controller {
 		else
 		{
 			// we have no chat yet
-			$result = array('status' => 'no chat', 'content' => '');
+			$result = array('status' => 'ok', 'content' => '');
 			
 			return json_encode($result);
 			exit();
