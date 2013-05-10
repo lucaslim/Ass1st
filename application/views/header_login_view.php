75389620e92cdda214929fdf9648a5eebd7c296e
Michael <mandarino013@hotmail.com>
2013-05-09 20:08:12 -0400

    Login View Menu / Footer / Header Update

--- application/views/header_login_view.php
+++ application/views/header_login_view.php
@@ -1,24 +1,4 @@
-<ul class="nav pull-right">
-	<li class="social">
-		<!-- facebook -->
-		<a href="https://www.facebook.com/pages/Team-Assist/522491314469799?ref=notif&notif_t=fbpage_presence"  target="_blank">
-			<i class="icon-facebook-sign icon-large" target="_blank"></i>
-		</a>
-	</li>
-	<li class="social">
-		<!-- twitter -->
-		<a href="https://twitter.com/teamassist_" class="twitter-sign" target="_blank">
-			<i class="icon-twitter icon-large"></i>
-		</a>
-	</li>
-	<!-- <li class="social"> -->
-		<!-- Google + -->
-		<!-- <a href="#" target="_blank"><i class="icon-google-plus-sign icon-large"></i></a> -->
-	<!-- </li> -->
-	<li class="social">
-		<!-- RSS -->
-		<a href="#" target="_blank"><i class="icon-rss icon-large"></i></a>
-	</li>			
+<ul class="nav pull-right">			
     <li>
       <button href="#signIn" class="btn btn-small btn-inverse" data-toggle="modal">Sign In</button>
     </li>
